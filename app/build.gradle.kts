93fd86711fef659bc2fc518f8933174362a07240
PANDATREE <xpl18382451971@gmail.com>
2026-06-17 22:27:29 +0900

    release: bump version to 1.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "app.marlboroadvance.mpvex"
     minSdk = 26
     targetSdk = 36
-    versionCode = 129
-    versionName = "1.2.9"
+    versionCode = 135
+    versionName = "1.3.5"
 
     vectorDrawables {
       useSupportLibrary = true
